6592a43c15be939a2559c925d3f9c86289bb265e
Aditya Kumar <adityapal1409@gmail.com>
2024-01-07 00:41:44 +0530

    feat: add functionality to extract text from image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tesseract from "tesseract.js";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [imagePath, setImagePath] = useState("");
+	const [text, setText] = useState("");
+	const [progress, setProgress] = useState(0);
+
+	const handleChange = (event) => {
+		setImagePath(URL.createObjectURL(event.target.files[0]));
+	};
+
+	const handleClick = () => {
+		Tesseract.recognize(imagePath, "eng", {
+			logger: ({ status, progress }) => {
+				setProgress(progress * 100);
+			},
+		})
+			.catch((err) => {
+				console.error(err);
+			})
+			.then((result) => {
+				// Get Confidence score
+				console.log("Result", result.data);
+				let confidence = result.confidence;
+
+				let text = result.data.text;
+				setText(text);
+			});
+	};
+
+	console.log("imagePath", imagePath);
+	return (
+		<div className="App">
+			<main className="App-main">
+				<h3>Actual image uploaded</h3>
+				<img src={imagePath} className="App-image" alt="logo" />
+
+				<h3>Extracted text</h3>
+				<div className="text-box">
+					<p> {text} </p>
+				</div>
+				<progress max={100} value={progress} />
+				<input type="file" onChange={handleChange} />
+				<button onClick={handleClick} style={{ height: 50 }}>
+					convert to text
+				</button>
+			</main>
+		</div>
+	);
 }
 
 export default App;
